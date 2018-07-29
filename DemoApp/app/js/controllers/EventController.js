13dcf4dcddc09641bd7d5e963d476d2da032ec2b
moin2672 <moin2672@gmail.com>
2018-07-29 10:41:22 +0530

    3.06.Demo Displaying Repeating Information 3m 6s

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -11,7 +11,30 @@ eventsApp.controller('EventController',
                 city:'Mountain View',
                 province:'CA'
             },
-            imageUrl: '/img/angularjs-logo.png'
+            imageUrl: '/img/angularjs-logo.png',
+            sessions:[
+                {
+                    name: 'Directives MasterClass',
+                    creatorName:'Bob Smith',
+                    duration:'1 hr',
+                    level:'Advanced',
+                    abstract:'In this session you will learn about directives.'
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName:'John Doe',
+                    duration:'30 mins',
+                    level:'Introductory',
+                    abstract:'In this session you will learn about the benefits of scopes.'
+                },
+                {
+                    name: 'Well behaved controllers',
+                    creatorName:'Jane doe',
+                    duration:'2 hr',
+                    level:'Intermediatory',
+                    abstract:'Controllers are the begining of everything that Angular does.'
+                }
+            ]
         };
     }
 );
