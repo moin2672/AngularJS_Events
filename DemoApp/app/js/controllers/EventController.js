8b411b082f400ab8c0b24ef821ddd84a252ecb80
moin2672 <moin2672@gmail.com>
2018-07-29 11:11:58 +0530

    3.07.Demo Handling Events 5m 35s

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -18,23 +18,33 @@ eventsApp.controller('EventController',
                     creatorName:'Bob Smith',
                     duration:'1 hr',
                     level:'Advanced',
-                    abstract:'In this session you will learn about directives.'
+                    abstract:'In this session you will learn about directives.',
+                    upVoteCount:0
                 },
                 {
                     name: 'Scopes for fun and profit',
                     creatorName:'John Doe',
                     duration:'30 mins',
                     level:'Introductory',
-                    abstract:'In this session you will learn about the benefits of scopes.'
+                    abstract:'In this session you will learn about the benefits of scopes.',
+                    upVoteCount:0
                 },
                 {
                     name: 'Well behaved controllers',
                     creatorName:'Jane doe',
                     duration:'2 hr',
                     level:'Intermediatory',
-                    abstract:'Controllers are the begining of everything that Angular does.'
+                    abstract:'Controllers are the begining of everything that Angular does.',
+                    upVoteCount:0
                 }
             ]
         };
+        $scope.upVoteSession = function(session){
+            session.upVoteCount++;
+        };
+        $scope.downVoteSession = function(session){
+            session.upVoteCount--;
+        };
+
     }
 );
