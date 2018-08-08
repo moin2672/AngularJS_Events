b1d231e6fc77aba7a81e40197bf028c4e07797ab
moin2672 <moin2672@gmail.com>
2018-08-08 15:55:23 +0530

    4.08.Using  Promises 1m 30s

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -1,13 +1,15 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope, eventData){
+    function EventController($scope, eventData, $log){
 
         $scope.sortOrder="name";
 
-        eventData.getEvent(function(event){
-            $scope.event=event;
-        });
+        eventData.getEvent()
+                 .success(function(event){$scope.event=event;})
+                 .error(function(data,status,headers,config){
+                     $log.warn(data,status,headers(),config);
+                 });
         $scope.upVoteSession = function(session){
             session.upVoteCount++;
         };
