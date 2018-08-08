a124f1ec87440adb7c10f0a2265ca8cfcc39c2cb
moin2672 <moin2672@gmail.com>
2018-08-08 15:57:48 +0530

    4.08.Using  Promises 1m 30sa

--- DemoApp/app/js/services/EventData.js
+++ DemoApp/app/js/services/EventData.js
@@ -1,6 +1,6 @@
 eventsApp.factory('eventData', function($http, $log){
     return{
-        getEvent:function(successcb){
+        getEvent:function(){
             return $http({method:'GET', url:'/data/event/1'});
         }
     };
