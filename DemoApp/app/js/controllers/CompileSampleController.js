d56ff76480aca8e200529ad1369c66abf46e3c82
moin2672 <moin2672@gmail.com>
2018-08-10 23:27:36 +0530

    4.13.Using Angular's compile Service 5m 34s

---
+++ DemoApp/app/js/controllers/CompileSampleController.js
@@ -0,0 +1,8 @@
+'use strict';
+
+eventsApp.controller("CompileSampleController", 
+    function CompileSampleController($scope, $compile){
+        $scope.appendDivToElement = function(markup){
+            return $compile(markup)($scope).appendTo(angular.element("#appendHere"));
+        }
+});
