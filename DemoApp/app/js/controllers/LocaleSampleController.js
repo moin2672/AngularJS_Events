7185f3993c03c5345eeef257324dfa5588865d6c
moin2672 <moin2672@gmail.com>
2018-08-11 10:28:55 +0530

    4.15.Using Angular's locale Service 2m 49s

---
+++ DemoApp/app/js/controllers/LocaleSampleController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+eventsApp.controller('LocaleSampleController',
+function LocaleSampleController($scope, $locale){
+    
+    console.log($locale);
+
+    $scope.myDate = Date.now();
+    $scope.myFormat = $locale.DATETIME_FORMATS.fullDate;
+    //$scope.myFormat = $locale.DATETIME_FORMATS.shortDate;
+
+    // https://code.angularjs.org/1.4.0/i18n
+});
