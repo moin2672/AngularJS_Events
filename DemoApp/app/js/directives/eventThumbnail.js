cade5531905e2d23ac99c8b8eeb99618af08c6ae
moin2672 <moin2672@gmail.com>
2018-08-12 19:12:22 +0530

    6.05.Isolating Directive Scope 4m 24s

--- DemoApp/app/js/directives/eventThumbnail.js
+++ DemoApp/app/js/directives/eventThumbnail.js
@@ -4,6 +4,9 @@ eventsApp.directive('eventThumbnail', function(){
     return {
         restrict:'E',
         replace: true, //to make the <event-thumbnail></event-thumbnail> of EventList.html invisible
-        templateUrl: '/templates/directives/eventThumbnail.html'
+        templateUrl: '/templates/directives/eventThumbnail.html',
+        scope:{
+            event:"=myEvent"
+        }
     }
 });
