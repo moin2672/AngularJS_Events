4760dbd21f593c83c1246ad822885416f205c3c6
moin2672 <moin2672@gmail.com>
2018-08-12 11:33:08 +0530

    6.04.Domain Specific Language via Custom Elements 5m 19s

---
+++ DemoApp/app/js/directives/eventThumbnail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+eventsApp.directive('eventThumbnail', function(){
+    return {
+        restrict:'E',
+        replace: true, //to make the <event-thumbnail></event-thumbnail> of EventList.html invisible
+        templateUrl: '/templates/directives/eventThumbnail.html'
+    }
+});
