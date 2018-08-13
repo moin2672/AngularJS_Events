5e4d80bb0f0557eb15f4b87a9117f9f6a7b9aad0
moin2672 <moin2672@gmail.com>
2018-08-13 20:55:57 +0530

    6.07.Handling Events with Directives 3m 13s

--- DemoApp/app/js/directives/upvote.js
+++ DemoApp/app/js/directives/upvote.js
@@ -6,7 +6,7 @@ eventsApp.directive('upvote', function(){
         replace: true, //to make the <event-thumbnail></event-thumbnail> of EventList.html invisible
         templateUrl: '/templates/directives/upvote.html',
         scope:{
-            upvote:"&",
+            upvote:"&", // to bind a local scope property to a function you want to execute in the parent scope
             downvote: "&",
             count:"=" // to pass integer directly 
         }
