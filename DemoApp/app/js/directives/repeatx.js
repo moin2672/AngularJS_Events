62e92369af018acd6b489f358825616136839da6
moin2672 <moin2672@gmail.com>
2018-08-15 11:08:33 +0530

    6.14.Using Compile to Transform the DOM 8m 44s_1

--- DemoApp/app/js/directives/repeatx.js
+++ DemoApp/app/js/directives/repeatx.js
@@ -1,8 +1,8 @@
-eventsApp.directive("repeatX", function(){
+eventsApp.directive("repeatX", function($compile){
     return {
         link: function(scope, element, attributes, controller){
             for(var i=0;i<Number(attributes.repeatX)-1;i++){
-                element.after(element.clone().attr('repeat-x',0));
+                element.after($compile(element.clone().attr('repeat-x',0))(scope));
             }
         }
     }
